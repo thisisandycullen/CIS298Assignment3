f126f87917e8cd2e93009776f879e9571823cd4a
David Barnes <dbarnes@kvcc.edu>
2015-11-08 22:11:06 -0500

    Moved the beverage list to a location that can be loaded at run time. Renamed the main activity from Wine to Beverage.

--- app/src/main/java/edu/kvcc/cis298/cis298assignment3/BeverageActivity.java
+++ app/src/main/java/edu/kvcc/cis298/cis298assignment3/BeverageActivity.java
@@ -3,7 +3,7 @@ package edu.kvcc.cis298.cis298assignment3;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class WineActivity extends AppCompatActivity {
+public class BeverageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
